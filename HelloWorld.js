4cf30a7e0134e0600b499ce09ea78c3938c4e0e8
Michal Zagdan <Michal Zagdan>
2017-02-24 01:14:17 +0100

    commit

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,16 +1,44 @@
 "use strict";
-//return the total number of smiling faces in the array
-function countSmileys(arr) {
-    let rightFaces = 0;
-    let rightEyes = [":", ";"];
-    let rightNoses = ["-", "~"];
-    let rightSmiles = [")", "D"];
-    for (let face of arr) {
-        if (face.match("^[:;][-~]?[\)D]$"))
-            rightFaces++;
+exports.__esModule = true;
+var G964 = (function () {
+    function G964() {
     }
-    return rightFaces;
-}
-exports.countSmileys = countSmileys;
-console.log(countSmileys([':D', ':~)', ';~D', ':)']));
-//# sourceMappingURL=HelloWorld.js.map
+    return G964;
+}());
+G964.movingShift = function (s, shift) {
+    var chunkSize = Math.floor(s.length / 5);
+    var chunk = '';
+    var results = [];
+    console.log('enter...');
+    for (var _i = 0, _a = s.slice(); _i < _a.length; _i++) {
+        var letter = _a[_i];
+        var char = letter.charCodeAt();
+        if (/^[a-z]+$/i.test(letter)) {
+            char += shift;
+            console.log(letter.charCodeAt());
+            console.log(char);
+        }
+        if (chunk.length < chunkSize) {
+            chunk += String.fromCharCode(char);
+        }
+        else {
+            chunk += String.fromCharCode(char);
+            results.push(chunk);
+            chunk = '';
+        }
+        shift++;
+    }
+    if (chunk.length > 0) {
+        results.push(chunk);
+    }
+    return results;
+};
+G964.demovingShift = function (arr, shift) {
+    // your code
+};
+exports.G964 = G964;
+var u, sol, v;
+u = "I should have known that you would have a perfect answer for me!!!";
+sol = ["J vltasl rlhr ", "zdfog odxr ypw", " atasl rlhr p ", "gwkzzyq zntyhv", " lvz wp!!!"];
+console.log(G964.movingShift(u, 1));
+console.log(sol);
--- HelloWorld.ts
+++ HelloWorld.ts
@@ -1,14 +1,51 @@
-//return the total number of smiling faces in the array
-export function countSmileys(arr: string[]) {
-    let rightFaces: number = 0;
+export class G964 {
 
-    for(let face of arr)
-    {
-        if(face.match("^[:;][-~]?[\)D]$") )
-        rightFaces++;
-    }    
+    public static movingShift = (s, shift) => {
+        let chunkSize: number = Math.floor(s.length / 5);
+        let chunk: string = '';
+        let results: string[] = []; 
 
-    return rightFaces;
+        console.log('enter...');
+        for(let letter of [...s])
+        {                   
+            let char: number =  letter.charCodeAt();
+            if(/^[a-z]+$/i.test(letter))
+            {
+                char += shift;
+                console.log(letter.charCodeAt());
+                console.log(char);
+            }
+            if(chunk.length < chunkSize) 
+            {
+                chunk += String.fromCharCode(char);
+            }
+            else
+            {
+                chunk += String.fromCharCode(char);
+                results.push(chunk);
+                chunk = '';
+            }
+
+            shift++;
+        }
+
+        if(chunk.length > 0)
+        {
+            results.push(chunk);
+        }
+
+        return results;
+    }
+
+    public static demovingShift = (arr, shift) => {
+        // your code
+    }
+
 }
 
-console.log(countSmileys([':D',':~)',';~D',':)']));
+
+var u, sol, v;
+        u = "I should have known that you would have a perfect answer for me!!!";
+        sol = ["J vltasl rlhr ", "zdfog odxr ypw", " atasl rlhr p ", "gwkzzyq zntyhv", " lvz wp!!!"];
+console.log(G964.movingShift(u, 1));
+console.log(sol);
