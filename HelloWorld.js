aa76506080a117cc38900195a90d04599b7476a9
Zagdan <MZagdan@corp.altegrity.com>
2017-02-24 10:37:03 +0100

    updated code

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,28 +1,24 @@
 "use strict";
-exports.__esModule = true;
 var G964 = (function () {
     function G964() {
     }
     return G964;
 }());
 G964.movingShift = function (s, shift) {
-    var chunkSize = Math.floor(s.length / 5);
+    var chunkSize = Math.ceil(s.length / 5);
     var chunk = '';
     var results = [];
-    console.log('enter...');
+    var charScope = 'z'.charCodeAt(0) - 'a'.charCodeAt(0) + 1;
     for (var _i = 0, _a = s.slice(); _i < _a.length; _i++) {
         var letter = _a[_i];
-        var char = letter.charCodeAt();
-        if (/^[a-z]+$/i.test(letter)) {
-            char += shift;
-            console.log(letter.charCodeAt());
-            console.log(char);
+        var char = letter.charCodeAt(0);
+        if (/^[a-zA-Z]{1}$/.test(letter)) {
+            var isSmallCap = char > 'Z'.charCodeAt(0);
+            shift = shift % charScope;
+            char += shift - (char + shift > (isSmallCap ? 'z' : 'Z').charCodeAt(0) ? charScope : 0);
         }
-        if (chunk.length < chunkSize) {
-            chunk += String.fromCharCode(char);
-        }
-        else {
-            chunk += String.fromCharCode(char);
+        chunk += String.fromCharCode(char);
+        if (chunk.length == chunkSize) {
             results.push(chunk);
             chunk = '';
         }
@@ -34,7 +30,23 @@ G964.movingShift = function (s, shift) {
     return results;
 };
 G964.demovingShift = function (arr, shift) {
-    // your code
+    var charScope = 'z'.charCodeAt(0) - 'a'.charCodeAt(0) + 1;
+    var output = '';
+    for (var _i = 0, arr_1 = arr; _i < arr_1.length; _i++) {
+        var chunk = arr_1[_i];
+        for (var _a = 0, _b = chunk.slice(); _a < _b.length; _a++) {
+            var letter = _b[_a];
+            var char = letter.charCodeAt(0);
+            if (/^[a-zA-Z]{1}$/.test(letter)) {
+                var isSmallCap = char > 'Z'.charCodeAt(0);
+                shift = shift % charScope;
+                char -= shift - (char - shift < (isSmallCap ? 'a' : 'A').charCodeAt(0) ? charScope : 0);
+            }
+            output += String.fromCharCode(char);
+            shift++;
+        }
+    }
+    return output;
 };
 exports.G964 = G964;
 var u, sol, v;
@@ -42,3 +54,5 @@ u = "I should have known that you would have a perfect answer for me!!!";
 sol = ["J vltasl rlhr ", "zdfog odxr ypw", " atasl rlhr p ", "gwkzzyq zntyhv", " lvz wp!!!"];
 console.log(G964.movingShift(u, 1));
 console.log(sol);
+console.log(G964.demovingShift(sol, 1));
+console.log(u);
--- HelloWorld.ts
+++ HelloWorld.ts
@@ -1,27 +1,24 @@
 export class G964 {
 
     public static movingShift = (s, shift) => {
-        let chunkSize: number = Math.floor(s.length / 5);
+        let chunkSize: number = Math.ceil(s.length / 5);
         let chunk: string = '';
         let results: string[] = []; 
+        let charScope: number = 'z'.charCodeAt(0) -'a'.charCodeAt(0) + 1;
 
-        console.log('enter...');
         for(let letter of [...s])
         {                   
-            let char: number =  letter.charCodeAt();
-            if(/^[a-z]+$/i.test(letter))
+            let char: number =  letter.charCodeAt(0);
+            if(/^[a-zA-Z]{1}$/.test(letter))
             {
-                char += shift;
-                console.log(letter.charCodeAt());
-                console.log(char);
+                let isSmallCap: boolean = char > 'Z'.charCodeAt(0);              
+                shift = shift % charScope;
+                char += shift - (char+shift > (isSmallCap ? 'z': 'Z').charCodeAt(0) ? charScope: 0);
             }
-            if(chunk.length < chunkSize) 
-            {
-                chunk += String.fromCharCode(char);
-            }
-            else
-            {
-                chunk += String.fromCharCode(char);
+
+            chunk += String.fromCharCode(char);            
+            if(chunk.length == chunkSize) 
+            {            
                 results.push(chunk);
                 chunk = '';
             }
@@ -38,14 +35,36 @@ export class G964 {
     }
 
     public static demovingShift = (arr, shift) => {
-        // your code
+
+        let charScope: number = 'z'.charCodeAt(0) -'a'.charCodeAt(0) + 1;
+        let output: string = '';
+
+        for(let chunk of arr)
+        {
+            for(let letter of [...chunk])
+            {
+                let char: number =  letter.charCodeAt(0);
+                if(/^[a-zA-Z]{1}$/.test(letter))
+                {
+                    let isSmallCap: boolean = char > 'Z'.charCodeAt(0);              
+                    shift = shift % charScope;
+                    char -= shift - (char-shift < (isSmallCap ? 'a': 'A').charCodeAt(0) ? charScope: 0);
+                }
+            
+                output += String.fromCharCode(char);
+                shift++;
+            }
+        }
+        return output;
     }
 
 }
 
 
 var u, sol, v;
-        u = "I should have known that you would have a perfect answer for me!!!";
+        u =    "I should have known that you would have a perfect answer for me!!!";
         sol = ["J vltasl rlhr ", "zdfog odxr ypw", " atasl rlhr p ", "gwkzzyq zntyhv", " lvz wp!!!"];
 console.log(G964.movingShift(u, 1));
 console.log(sol);
+console.log(G964.demovingShift(sol, 1));
+console.log(u);
